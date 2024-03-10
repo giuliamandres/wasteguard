a3df4aa85fff49371340a2c524119fff033e1f56
giuliamandres <93037838+giuliamandres@users.noreply.github.com>
2024-03-10 20:59:27 +0200

    First commit

---
+++ wasteguard/android/app/src/main/kotlin/com/example/wasteguard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wasteguard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ wasteguard/lib/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(70.0, 200.0, 70.0, 100.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Text('WasteGuard', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.teal))),
+                  SizedBox(width: 5),
+                  Icon(Icons.warning_amber_rounded, size: 35.0)
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black), borderRadius: BorderRadius.circular(7)),
+                    child: const TextField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black), borderRadius: BorderRadius.circular(7)),
+                    child: const TextField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  ElevatedButton(onPressed: () {}, child: const Text('Login')),
+                  const SizedBox(height: 16),
+                  Text.rich(
+                      TextSpan(
+                          children: [
+                            TextSpan(text: 'Create Account', style: TextStyle(color: Colors.teal.shade800, fontWeight: FontWeight.bold)),
+                            const WidgetSpan(child: SizedBox(width: 40)),
+                            TextSpan(text: 'Forgot Password?', style: TextStyle(color: Colors.teal.shade800, fontWeight: FontWeight.bold))
+                          ]
+                      )
+                  )
+
+                ],
+              ),
+            )
+          ]
+      ),
+    );
+  }
+
+}
